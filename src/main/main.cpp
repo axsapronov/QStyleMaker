8ec90a84e84e2af773a6eccdb14a1584b8d0ebac
Sapronov Alex <warmonger72@gmail.com>
2013-07-08 12:12:55 +0700

    [ADD] ADd encoding

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,11 +1,28 @@
 #include "maindialog.h"
 #include <QApplication>
 
+#include <QTextCodec>
+#include <QTranslator>
+#include <QLocale>
+
+
+#include "src/defines/defines.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    a.setApplicationName(D_PROG_NAME);
+
+    QTextCodec * codec = QTextCodec::codecForName("UTF-8"); // set encoding for prog
+    QTextCodec::setCodecForCStrings(codec);
+    QTextCodec::setCodecForLocale(codec);
+    QTextCodec::setCodecForTr(codec);
+
+
     MainDialog w;
     w.show();
-    
+
+
     return a.exec();
 }
