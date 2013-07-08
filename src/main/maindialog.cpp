8cf52cdb6b4f2d3b420d26cd63414e1309b4ab74
Sapronov Alex <warmonger72@gmail.com>
2013-07-08 11:22:05 +0700

    [ADD] add slots and signal

--- src/main/maindialog.cpp
+++ src/main/maindialog.cpp
@@ -1,11 +1,17 @@
 #include "maindialog.h"
 #include "ui_maindialog.h"
 
+#include <QDebug>
+
 MainDialog::MainDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::MainDialog)
 {
     ui->setupUi(this);
+
+
+
+    createConnects();
 }
 //------------------------------------------------------------------------------
 MainDialog::~MainDialog()
@@ -25,6 +31,34 @@ void MainDialog::changeEvent(QEvent *e)
     }
 }
 //------------------------------------------------------------------------------
+void MainDialog::createConnects()
+{
+    connect(ui->pBSave, SIGNAL(clicked()), this, SLOT(slotSaveStyle()));
+    connect(ui->pBQuit, SIGNAL(clicked()), this, SLOT(slotQuit()));
+    connect(ui->pBTest, SIGNAL(clicked()), this, SLOT(slotTest()));
+
+    connect(ui->listWidget, SIGNAL(currentRowChanged(int)), ui->stackedWidget, SLOT(setCurrentIndex(int)));
+    connect(ui->listWidget, SIGNAL(itemSelectionChanged()), this, SLOT(slotUpdatePreview()));
+}
 //------------------------------------------------------------------------------
+void MainDialog::slotQuit()
+{
+
+}
 //------------------------------------------------------------------------------
+void MainDialog::slotTest()
+{
+
+}
+//------------------------------------------------------------------------------
+void MainDialog::slotSaveStyle()
+{
+
+}
+//------------------------------------------------------------------------------
+void MainDialog::slotUpdatePreview()
+{
+    qDebug() << "update preview";
+}
+
 //------------------------------------------------------------------------------
--- src/main/maindialog.h
+++ src/main/maindialog.h
@@ -10,16 +10,25 @@ class MainDialog;
 class MainDialog : public QDialog
 {
     Q_OBJECT
-    
+
 public:
     explicit MainDialog(QWidget *parent = 0);
     ~MainDialog();
-    
+
 protected:
     void changeEvent(QEvent *e);
-    
+
+private slots:
+    void slotSaveStyle();
+    void slotTest();
+    void slotQuit();
+
+    void slotUpdatePreview();
+
 private:
     Ui::MainDialog *ui;
+
+    void createConnects();
 };
 
 #endif // MAINDIALOG_H
